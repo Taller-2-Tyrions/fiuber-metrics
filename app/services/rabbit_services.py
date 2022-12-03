7816330733320d3499076f93c0e2880ebaa4bd09
Lucas Veron <veron_lucas@hotmail.com>
2022-12-02 21:00:53 -0300

    endpoint y metricas de payments y voyages

--- app/services/rabbit_services.py
+++ app/services/rabbit_services.py
@@ -3,12 +3,12 @@ from ..crud import crud
 from ..database.mongo import db
 
 def metrics_users_process_function(msg):
-  print("Metrics users processing")
+  print("Metrics users processing init ...")
   print("[x] Received: " + str(msg))
 
-  crud.insert_user_metric(db, msg)
+  crud.insert_metric(db, msg)
 
-  print("Metrics users processing finished");
+  print("Metrics users processing finished!");
   return;
 
 # create a function which is called on incoming messages
@@ -18,6 +18,11 @@ def callback(ch, method, properties, body):
 
 def run_rabbit_service():
   print("Starting rabbit services...")
+
+  print("Check empty database ...")
+  crud.insert_users_metric_empty()
+  crud.insert_payments_metric_empty()
+  crud.insert_voyages_metric_empty()
 
   # Access the CLODUAMQP_URL environment variable and parse it (fallback to localhost)
   url = os.environ.get('CLOUDAMQP_URL', 'amqps://wjcumqjk:5YBAhdzlge0w0Itf5t94VPWy_h0xs8cX@chimpanzee.rmq.cloudamqp.com/wjcumqjk')
